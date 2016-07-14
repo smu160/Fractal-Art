5bed082a83cf4469e2174e7947338f7047e446b6
Saveliy <Saveliy58@yahoo.com>
2016-07-14 06:00:44 +0300

    Updated to use class that offers complex data type

--- Mandelbrot set/src/MandelbrotSetGenerator.java
+++ Mandelbrot set/src/MandelbrotSetGenerator.java
@@ -1,87 +1,72 @@
-import java.awt.BorderLayout;
-import java.awt.Dimension;
-import java.awt.Graphics;
-import java.awt.image.BufferedImage;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
- 
-public class MandelbrotSetGenerator extends JFrame {
- 
-    private final int MAX_NUM_OF_ITERATIONS = 1000;
-    private final int HEIGHT = 600;
-    private final int WIDTH = 600;
-    
-    DrawingPanel drawingPanel;
-    BufferedImage Image;
-    
-    public MandelbrotSetGenerator() {
-    	setGUI();
-    	setDrawingPanel();
-    }
-    
-    /**
-     *     z = x + iy 
-     * ==> z^2 = (x + iy)(x + iy)
-     * ==> z^2 = x^2 + 2ixy + (-y^2)
-     * ==> z^2 = x^2 + i2xy - y^2
-     * 
-     *     c = x_0 + iy_0
-     * 
-     *     x = Re(z^2 + c) = x^2 - y^2 + x_0 
-     *     y = Im(z^2 + c) = 2xy + y_0
-     */
-	private int computeIterations(double complexNumRealPart, double complexNumImagPart) {
-    	
-    	double x = 0.0;
-    	double y = 0.0;
-    	int iterationCounter = 0;
-    	while(x * x + y * y < 4 && iterationCounter < MAX_NUM_OF_ITERATIONS) {
-    		double xtemp = x * x - y * y + x_0;
-    		y = 2 * x * y + y_0;
-    		x = xtemp;
+import java.awt.Color;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
 
-    		// TODO Add Comment
-    		if(iterationCounter >= MAX_NUM_OF_ITERATIONS) {
-    			return MAX_NUM_OF_ITERATIONS;
-    		}
-    		iterationCounter++;
-    	}
-    	return iterationCounter;
-    } 
-    
-    // Initialize the drawing panel and the Image and display them
-    private void setDrawingPanel() {
-    	drawingPanel = new DrawingPanel();
-		Image = new BufferedImage(WIDTH, HEIGHT, BufferedImage.TYPE_INT_RGB);
-		drawingPanel.setVisible(true);
-		this.add(drawingPanel, BorderLayout.CENTER);
-	}
+public class MandelbrotSetGenerator {
 
-	// Creates a Frame and sets its initial properties
-    private void setGUI() {
-    	this.setTitle("Fractal Generator");
-    	this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-    	this.setSize(WIDTH, HEIGHT);
-    	this.setResizable(false);
-    	this.setLocationRelativeTo(null);
-    	this.setVisible(true);
-	}
-
-    // Creates a component that you can actually draw on
-    private class DrawingPanel extends JPanel{
-      
-    	@Override public Dimension getPreferredSize() {
-    		return new Dimension(WIDTH, HEIGHT);
-    	}
-      
-        // Draws the actual fractal 
-    	@Override public void paint(Graphics drawObject) {
-    	  	drawObject.drawImage(Image, 0, 0, null);
-        }
-    }
-    
-	public static void main(String[] args) {
-		new MandelbrotSetGenerator();
-    } // end main
+	//static int[] histogram = new int[n];
 	
+    // return number of iterations to check if c = a + ib is in Mandelbrot set
+    public static int mand(Complex z0, int max) {
+        Complex z = z0;
+        for (int t = 0; t < max; t++) {
+            if (z.abs() > 2.0) return t;
+            z = z.times(z).plus(z0);
+        }
+        return max;
+    }
+
+    public static void main(String[] args)  {
+        
+    	int total = 0;
+    	double xc   = 0.0;
+        double yc   = 0.0;
+        double size = 5;
+
+        int n = 1440;   // create an n * n sized window
+        int max = 256;  // maximum number of iterations
+        
+        int[] histogram = new int[n];
+        
+        // Read in color map
+        File file = new File("mandel.txt");
+        Color[] colors = new Color[max];
+		Scanner in;
+		try {
+			in = new Scanner(file);
+			while(in.hasNext()) { 
+				for (int row = 0; row < max; row++) {
+					int r = in.nextInt();
+		            int g = in.nextInt();
+		            int b = in.nextInt();
+		            colors[row] = new Color(r, g, b);
+                }
+            }
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+        Picture picture = new Picture(n, n);
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                double x = xc - size / 2 + size * i / n;
+                double y = yc - size / 2 + size * j / n;
+                Complex z0 = new Complex(x, y);
+                
+                for (i = 0; i < max; i++) {
+                	total += histogram[i];
+                }
+                double hue = 0.0;
+                for (i = 0; i < mand(z0, max - 1); i++) {
+                  hue += histogram[i] / (double)total; 	// Must be floating-point division.
+                }
+                
+               // int colorIndex = mand(z0, max - 1);
+                picture.set(i, (n - 1 - j), colors[(int) hue]);
+            }
+        }
+        picture.show();
+
+    } // end main
 } // end class
